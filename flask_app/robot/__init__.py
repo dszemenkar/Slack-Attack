f32b0fb9868b5a63fb38f99de23b488ab1888450
David Szemenkar <hello@appkompaniet.com>
2021-04-16 14:48:26 +0200

    Added users

--- flask_app/robot/__init__.py
+++ flask_app/robot/__init__.py
@@ -17,9 +17,11 @@ Migrate(app, db)
 
 from robot.core.views import core
 from robot.messages.views import messages
+from robot.users.views import users
 from robot.error_pages.handlers import error_pages
 
 
 app.register_blueprint(core)
 app.register_blueprint(messages)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
